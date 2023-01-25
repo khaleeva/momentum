0fe10abe4859b0bee4f7ebb9bccca1965544f631
eugeniakhaleeva <khaleeva20990@gmail.com>
2023-01-25 23:31:03 +0100

    init: start project, added fn time

---
+++ src/js/time.js
@@ -0,0 +1,24 @@
+
+const time = document.querySelector('.time'),
+    dateElem = document.querySelector('.date')
+
+const weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+const showTime = () => {
+    const date = new Date();
+    time.textContent = date.toLocaleTimeString()
+    showDate()
+    setTimeout(showTime, 1000)
+}
+
+const showDate = () => {
+    const date = new Date();
+    const options = {month: 'long', day: 'numeric'};
+    const currentDate = date.toLocaleDateString('en-En', options);
+    const day = date.getDay()
+    dateElem.textContent = `${weekDays[day]}, ${currentDate}`
+}
+
+showTime()
+
+
