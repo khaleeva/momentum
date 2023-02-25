f953605fae5544a7c90912c4d90198475baa1d67
eugeniakhaleeva <khaleeva20990@gmail.com>
2023-02-25 23:28:53 +0100

    fix: fix todo

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,15 +1,11 @@
 import i18next from 'i18next';
 import state from "./settings";
-import { v4 as uuidv4 } from 'uuid';
-
+import {v4 as uuidv4} from 'uuid';
 
 
 let todoList = {
     list: [],
-    checked: []
 }
-
-
 
 
 let taskValue = ''
@@ -25,22 +21,11 @@ function setTodoLocalStorage() {
 window.addEventListener("beforeunload", setTodoLocalStorage)
 
 
+const savedTodoList = JSON.parse(localStorage.getItem('todo'));
 
-
-    const savedTodoList = JSON.parse(localStorage.getItem('todo'));
-
-    if (savedTodoList) {
-        todoList.list = savedTodoList.list
-        todoList.checked = savedTodoList.checked
-
-
-
-
-
-    }
-
-
-
+if (savedTodoList) {
+    todoList.list = savedTodoList.list
+}
 
 
 task.addEventListener('input', function () {
@@ -63,7 +48,7 @@ document.querySelector('.todo-btn').addEventListener('click', () => {
 
 function addTask(task, id) {
     if (task) {
-        todoList.list.push({'task': task, 'id': id});
+        todoList.list.push({'task': task, 'id': id, 'checked': false});
         showTasks()
     }
 
@@ -71,53 +56,45 @@ function addTask(task, id) {
 
 function addRemoveButtonHandlers() {
     document.querySelectorAll('.remove-btn').forEach(remove => remove.addEventListener('click', (e) => {
-        removeTask(todoList.list, e.target.previousElementSibling.firstElementChild.id)
-        removeTask(todoList.checked, e.target.previousElementSibling.firstElementChild.id)
-
+        removeTask(e.target.previousElementSibling.firstElementChild.id)
     }));
 }
 
 
 function checkTask() {
-        document.querySelectorAll('.todo-check').forEach(check =>
-            check.addEventListener('change', function (e) {
-                const isChecked = check.checked;
-                const checkElem = check.value
-
-                if (isChecked) {
-                    e.target.nextElementSibling.style.textDecoration = 'line-through'
-                    todoList.checked.push(checkElem)
-
-                } else {
-                    e.target.nextElementSibling.style.textDecoration = 'none'
-                    const index = todoList.checked.indexOf(checkElem);
-                    if (index !== -1) {
-                        removeTask(todoList.checked, index)
+    document.querySelectorAll('.todo-check').forEach(check =>
+        check.addEventListener('change', function (e) {
+            const isChecked = check.checked;
+            const checkId = check.id
+            todoList.list.forEach(l => {
+                if (l.id === checkId) {
+                    if (isChecked) {
+                        e.target.nextElementSibling.style.textDecoration = 'line-through'
+                        l.checked = true
+                    } else {
+                        e.target.nextElementSibling.style.textDecoration = 'none'
+                        l.checked = false
                     }
                 }
             })
-        )
+        })
+    )
 
 }
 
 function checkTaskFromLS() {
     if (savedTodoList) {
-        savedTodoList.checked.forEach((id) => {
-            setTimeout(() => {
-                const checkItems = document.querySelector(`input[value="${id}"]`);
-                console.log(checkItems)
+        savedTodoList.list.forEach((item) => {
+            if (item.checked) {
+                const checkItems = document.querySelector(`input[value="${item.id}"]`);
                 if (checkItems) {
                     checkItems.checked = true;
                     checkItems.nextElementSibling.style.textDecoration = 'line-through';
                 }
-            }, 100); // задержка в 100 мс
+            }
         });
     }
 }
-
-
-
-
 
 
 export function showTasks() {
@@ -130,14 +107,15 @@ export function showTasks() {
     } else {
         todoTitle.innerText = i18next.t('todolist', {lng: state.language})
         todo.innerHTML =
-            todoList.list.map((i, index) =>{
-                return `<div class="todo-items">
+            todoList.list.map((i, index) => {
+                    return `<div class="todo-items">
                         <div class="todo-item">
                             <input type="checkbox" id=${i.id} value=${i.id} class="custom-checkbox todo-check">
                             <label for=${i.id} class="check-task">${index + 1}. ${i.task}</label>
                         </div>
                         <div class="remove-btn"></div>
-                  </div>`}
+                  </div>`
+                }
             ).join('')
     }
 
@@ -147,11 +125,8 @@ export function showTasks() {
 }
 
 
-
-
 function removeTask(arr, index) {
-    console.log(index)
-    arr.splice(index, 1);
+    todoList.list.splice(index, 1);
     showTasks()
 }
 
